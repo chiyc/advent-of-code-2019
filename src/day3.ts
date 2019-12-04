fba74ba42ef439c47df140884f99b32894eb546a
Chi Choi <cychoi@zoho.com>
2019-12-04 17:17:04 -0500

    clean up day 3 and add types

--- src/day3.ts
+++ src/day3.ts
@@ -1,35 +1,6 @@
 "use strict";
 
 import * as fs from "fs";
-// import * as readline from "readline";
-// const fs = require("fs");
-// const readline = require("readline");
-// import { once } from "events";
-// import * as events from "events";
-// const { once } = require("events");
-
-// (async function day3() {
-//   try {
-//     const rl = readline.createInterface({
-//       input: fs.createReadStream("input/day3.input"),
-//       crlfDelay: Infinity
-//     });
-
-//     rl.on("line", (line) => {
-//       console.log(line);
-//     });
-
-//     // await events.once(rl, "close");
-
-//     for await (const line of rl) {
-//       // Each line in input.txt will be successively available here as `line`.
-//       console.log(`Line from file: ${line}`);
-//     }
-//     console.log("Hello?");
-//   } catch (err) {
-//     console.error(err);
-//   }
-// })();
 interface Segment {
   direction: string;
   length: number;
@@ -38,6 +9,28 @@ interface Segment {
 interface Point {
   x: number;
   y: number;
+}
+
+enum Direction {
+  Up = "U",
+  Down = "D",
+  Left = "L",
+  Right = "R",
+}
+
+enum Wire {
+  A,
+  B,
+}
+
+enum WirePositionState {
+  A,
+  B,
+  Intersected,
+}
+interface WireMapPosition {
+  state: WirePositionState;
+  distance: number;
 }
 
 const input = fs.readFileSync("input/day3.input", "utf-8");
@@ -53,27 +46,21 @@ const [wireA, wireB] = input
       }))
   ));
 
-function setWireMapValue(currentPosition: any, currentPositionDistance: any, wireName: any) {
-
+function setWireMapValue(currentPosition: WireMapPosition | undefined, currentPositionDistance: number, wireName: Wire): WireMapPosition {
   let distance;
   let state;
-  if (currentPosition === undefined && wireName === 'A') {
-    state = 'A';
-    distance = currentPositionDistance;
 
-  } else if (currentPosition === undefined && wireName === 'B') {
-    state = 'B';
+  if (currentPosition === undefined) {
+    state = wireName === Wire.A ? WirePositionState.A : WirePositionState.B;
     distance = currentPositionDistance;
-
-  } else if (currentPosition !== undefined && currentPosition.state === 'A' && wireName === 'B') {
-    state = 'intersection';
+  } else if (currentPosition !== undefined && currentPosition.state === WirePositionState.A && wireName === Wire.B) {
+    state = WirePositionState.Intersected;
     distance = currentPosition.distance + currentPositionDistance;
-
-  } else if (currentPosition !== undefined && currentPosition.state === 'A') {
-    state = 'A overlapped';
+  } else if (currentPosition !== undefined && currentPosition.state === WirePositionState.A) {
+    state = WirePositionState.A;
     distance = currentPosition.distance;
-  } else  if (currentPosition !== undefined && currentPosition.state === 'B') {
-    state = 'B overlapped';
+  } else {
+    state = WirePositionState.B;
     distance = currentPosition.distance;
   }
 
@@ -83,7 +70,7 @@ function setWireMapValue(currentPosition: any, currentPositionDistance: any, wir
   };
 }
 
-function getWireMap(wireSegments: Segment[], position: Point, distance: number, wireMap: any, wireName: string): any {
+function getWireMap(wireSegments: Segment[], position: Point, distance: number, wireMap: {[key: string]: WireMapPosition}, wireName: Wire): {[key: string]: WireMapPosition} {
   if (wireSegments.length === 0) {
     return wireMap;
   }
@@ -91,28 +78,28 @@ function getWireMap(wireSegments: Segment[], position: Point, distance: number, 
   const currentSegment = wireSegments[0];
   wireSegments.shift();
   switch(currentSegment.direction) {
-    case "U":
+    case Direction.Up:
       for (let i = 1; i <= currentSegment.length; i++) {
         const nextPoint = `${position.x},${position.y+i}`;
         wireMap[nextPoint] = setWireMapValue(wireMap[nextPoint], distance+i, wireName);
       }
       nextPosition.y += currentSegment.length;
       break;
-    case "D":
+    case Direction.Down:
       for (let i = 1; i <= currentSegment.length; i++) {
         const nextPoint = `${position.x},${position.y-i}`;
         wireMap[nextPoint] = setWireMapValue(wireMap[nextPoint], distance+i, wireName);
       }
       nextPosition.y -= currentSegment.length;
       break;
-    case "R":
+    case Direction.Right:
       for (let i = 1; i <= currentSegment.length; i++) {
         const nextPoint = `${position.x+i},${position.y}`;
         wireMap[nextPoint] = setWireMapValue(wireMap[nextPoint], distance+i, wireName);
       }
       nextPosition.x += currentSegment.length;
       break;
-    case "L":
+    case Direction.Left:
       for (let i = 1; i <= currentSegment.length; i++) {
         const nextPoint = `${position.x-i},${position.y}`;
         wireMap[nextPoint] = setWireMapValue(wireMap[nextPoint], distance+i, wireName);
@@ -124,57 +111,16 @@ function getWireMap(wireSegments: Segment[], position: Point, distance: number, 
   return getWireMap(wireSegments, nextPosition, distance + currentSegment.length, wireMap, wireName);
 }
 
-
-
-const wireAMap = getWireMap(wireA, {x: 0, y: 0}, 0, {}, 'A');
-// console.log(wireAMap);
-// console.log(wireAMap);
-// console.log(wireA.length);
-// console.log(Object.keys(wireAMap).length);
-// const wireAMap2 = getWireMap([{direction: "R", length: 1009}], {x:1009, y:0}, wireAMap, 'hello');
-// console.log(wireAMap2[0]);
-// console.log("STARTING WIRE B", wireB.slice(0,10));
-
-
-const combinedWireMap = getWireMap(wireB, {x: 0, y: 0}, 0, wireAMap, 'B');
-// console.log(wireB.length + Object.keys(wireAMap).length);
-// console.log(Object.keys(combinedWireMap).length);
-
-// console.log(combinedWireMap);
-
-let uniqueValue: any = {};
-console.log(Object.keys(combinedWireMap).map((key) => combinedWireMap[key]).forEach(value => uniqueValue[value] = true));
-console.log(uniqueValue);
-
-// console.log(Object.keys(combinedWireMap).join('|'));
-
-
-
-
-
-// const intersections = Object.keys(combinedWireMap)
-//   .filter((point) => (
-//     combinedWireMap[point] === 2
-//   ));
-
-
-// console.log(intersections);
-// console.log(intersections.map((point) => (
-//   point.split(',').map((value) => Math.abs(Number.parseInt(value))).reduce((a,b) => a+b)
-// )));
-
-
+const wireAMap = getWireMap(wireA, {x: 0, y: 0}, 0, {}, Wire.A);
+const combinedWireMap = getWireMap(wireB, {x: 0, y: 0}, 0, wireAMap, Wire.B);
 
 // SHOULD BE THE ANSWER
 const intersections = Object.keys(combinedWireMap)
   .filter((point) => (
-    combinedWireMap[point].state === 'intersection'
+    combinedWireMap[point].state === WirePositionState.Intersected
   ));
 
-// console.log(combinedWireMap);
-console.log(intersections);
-
-const intersectionDistances = intersections
+const intersectionManhattanDistances = intersections
   .map((point) =>
     point.split(',').map((value) => Math.abs(Number.parseInt(value))).reduce((a,b) => a+b)
   );
@@ -183,22 +129,8 @@ const intersectionTraveledLengths = intersections
   .map((intersection) => (
     combinedWireMap[intersection].distance
   ));
-  // .map((point) => ({
-  //   point,
-  //   distance: point.split(',').map((value) => Math.abs(Number.parseInt(value))).reduce((a,b) => a+b)
-  // }));
 
-// intersectionDistances.sort(function(a, b) {
-//   if (a.distance < b.distance) {
-//     return -1;
-//   }
-//   if (a.distance > b.distance) {
-//     return 1;
-//   }
-//   return 0;
-// });
-// console.log(intersectionDistances.slice(0, 10));
-const smallestManhattanDistance = Math.min(...intersectionDistances);
+const smallestManhattanDistance = Math.min(...intersectionManhattanDistances);
 const smallestIntersectionTraveledLengths = Math.min(...intersectionTraveledLengths);
 
 console.log(`Day 3 - Part 1: ${smallestManhattanDistance}`); // 721!!!!
